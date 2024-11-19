5ecc7cb846566f9a87c4af2bf1009cdd3f5a5d66
Eamon Tracey <etracey@nd.edu>
2024-11-19 14:48:11 -0500

    correct pins for lora bonnet

--- src/misc/control_script.py
+++ src/misc/control_script.py
@@ -4,8 +4,8 @@ import busio
 import digitalio
 
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-cs = digitalio.DigitalInOut(board.D5)
-rst = digitalio.DigitalInOut(board.D6)
+cs = digitalio.DigitalInOut(board.CE1)
+rst = digitalio.DigitalInOut(board.D25)
 rfm95w = adafruit_rfm9x.RFM9x(spi, cs, rst, 915)
 
 while True:
