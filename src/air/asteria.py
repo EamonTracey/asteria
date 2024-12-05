7e5d5e253fae30ba377b6d6fd7cc77925cb7796e
eamontracey <etracey@nd.edu>
2024-12-05 15:45:03 -0500

    10hz generally and camera to loop

--- src/air/asteria.py
+++ src/air/asteria.py
@@ -19,7 +19,7 @@ class Asteria:
 
     def __init__(self, name: str):
         # Loop!
-        self._loop = Loop(1)
+        self._loop = Loop(10)
         loop_state = self._loop.state
 
         # Connect to the I2C and SPI buses.
@@ -29,44 +29,49 @@ class Asteria:
         # BNO085.
         bno085_component = BNO085Component(i2c)
         bno085_state = bno085_component.state
-        self._loop.add_component(bno085_component, 1)
+        self._loop.add_component(bno085_component, 10)
 
         # Lidar.
         lidar_component = LidarComponent(i2c)
         lidar_state = lidar_component.state
-        self._loop.add_component(lidar_component, 1)
+        self._loop.add_component(lidar_component, 10)
 
         # MCP9808.
         mcp9808_component = MCP9808Component(i2c, 0x1C)
         mcp9808_state = mcp9808_component.state
-        self._loop.add_component(mcp9808_component, 1)
+        self._loop.add_component(mcp9808_component, 10)
 
         # RFM95W.
         rfm95w_component = RFM95WComponent(spi, board.CE1, board.D14,
                                            bno085_state, lidar_state,
                                            mcp9808_state)
         rfm95w_state = rfm95w_component.state
-        self._loop.add_component(rfm95w_component, 1)
+        self._loop.add_component(rfm95w_component, 2)
 
         # Temperature Regulation.
         temperature_regulation_component = TemperatureRegulationComponent(
             mcp9808_state)
-        self._loop.add_component(temperature_regulation_component, 1)
+        self._loop.add_component(temperature_regulation_component, 10)
 
         # Stage.
         stage_component = StageComponent(loop_state, lidar_State)
         stage_state = stage_component.state
+        self._loop.add_component(stage_state, 10)
 
         # Control.
         control_component = ControlComponent(board.D12, board.D13,
                                              bno085_state, rfm95w_state,
                                              stage_state)
-        self._loop.add_component(control_component, 1)
+        self._loop.add_component(control_component, 10)
+
+        # Camera.
+        camera_component = CameraComponent()
+        self._loop.add_component(camera_component, 1)
 
         # Log
         log_component = LogComponent(f"{name}.csv", loop_state, bno085_state,
                                      lidar_state, mcp9808_state, stage_state)
-        self._loop.add_component(log_component, 1)
+        self._loop.add_component(log_component, 10)
 
     def run(self, steps: int):
         self._loop.run(steps)
